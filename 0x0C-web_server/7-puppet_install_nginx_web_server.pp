1d4fb9d49539f090a605974aef9c4bb048f711ea
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-30 01:45:07 +0100

    feat: setup puppet manifest to confure nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,47 @@
+# install and configure nginx on an ubuntu server
+
+package { 'nginx':
+  ensure => installed,
+  provider => 'apt'
+}
+
+file { '/var/www/html/':
+  ensure => "directory",
+  before => Service['nginx']
+}
+
+file { '/var/www/html/index.html':
+  ensure => "file",
+  content => "Hello World!",
+  require => File['/var/www/html/']
+}
+
+file { '/etc/nginx/sites-enabled/default':
+  ensure => "file",
+  content => 'server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+
+        location / {
+                try_files $uri $uri/ =404;
+        }
+  }',
+  require => Package['nginx'],
+  before => Service['nginx']
+
+}
+
+service { 'nginx':
+  ensure => 'running',
+  require => Package['nginx']
+}
