1f8d62c4cff2582448f35531a4cf7e4e6e9b1489
Awe Ayomidipupo <pupoawe@gmail.com>
2023-04-24 14:37:42 +0100

    feat: add script to export data from API as csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This is a python script to fetch data from
+an API and display the data formatted. This
+script takes in exactly one argument, which is the
+id of the user to be fetched
+"""
+import csv
+import requests
+import sys
+
+
+def fetch_todos():
+    """ Fetch todos for the specified employee,
+    from the jsonplaceholder API
+    """
+    employee_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    user = requests.get(url).json()
+    todos = requests.get(url + "/todos").json()
+    return {"user": user, "todos": todos}
+
+
+def export_as_csv(user_data):
+    """Formats data fetched from the API and
+    exports as a csv
+    """
+    employee_id = sys.argv[1]
+    with open("{}.csv".format(employee_id), mode="w", newline="") as csvFile:
+        fieldnames = ["id", "username", "completed", "title"]
+        writer = csv.DictWriter(
+            csvFile, fieldnames=fieldnames, extrasaction="ignore", quoting=csv.QUOTE_ALL)
+
+        todos = user_data.get("todos")
+        username = user_data.get("user").get("username")
+        for todo in todos:
+            writer.writerow({**todo, "username": username},)
+
+
+if __name__ == "__main__":
+    user_data = fetch_todos()
+    export_as_csv(user_data)
